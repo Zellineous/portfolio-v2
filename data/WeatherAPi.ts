d5ca54e6e05d52108b4f58829e9f928ca98cc509
Raymond Zuo <raymond101combatarms@gmail.com>
2026-02-26 17:16:55 -0800

    removed API Key

--- data/WeatherAPi.ts
+++ data/WeatherAPi.ts
@@ -1,4 +1,4 @@
-const apiKey = '56a4dee59147326b084dd94db9c71d2b';
+const apiKey = '';
 const apiUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
 
 const locationInput = document.getElementById('locationInput') as HTMLInputElement;
